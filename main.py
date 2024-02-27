9a157c07d27cff7ee76f7a7e37179997e14db4d1
Juanes <juan.rivero@aireeterno.com>
2024-02-26 19:50:58 -0500

    creando el dashboard y demas

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
-from starlette.responses import RedirectResponse
-from starlette.status import HTTP_302_FOUND
-from fastapi import FastAPI
 from woocommerce import API
+from database.connection import execute_query
 
 app = FastAPI()
-
 templates = Jinja2Templates(directory="templates")
+
 
 @app.get("/")
 def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
+
 @app.get("/")
 async def read_root():
     return {"message": "Hello, welcome to the WooCommerce API with FastAPI!"}
+
 
 @app.get("/products/{product_id}")
 async def read_product(product_id: int):
@@ -23,10 +23,11 @@ async def read_product(product_id: int):
         url="https://rizosfelices.co",
         consumer_key="ck_71555cb7c8c3489cf2ea8b231cff6ea704001ac9",
         consumer_secret="cs_8cb1c962a51cd4feac1894987d5d8ccd5aa078f3",
-        version="wc/v3"
+        version="wc/v3",
     )
-    product = wcapi.get("products/{}".format(product_id)).json()
+    product = wcapi.get(f"products/{product_id}").json()
     return product
+
 
 @app.get("/products/")
 async def read_products():
@@ -34,7 +35,7 @@ async def read_products():
         url="https://rizosfelices.co",
         consumer_key="ck_71555cb7c8c3489cf2ea8b231cff6ea704001ac9",
         consumer_secret="cs_8cb1c962a51cd4feac1894987d5d8ccd5aa078f3",
-        version="wc/v3"
+        version="wc/v3",
     )
     products = wcapi.get("products").json()
     return products
@@ -46,25 +47,34 @@ async def get_customers_from_orders():
         url="https://rizosfelices.co",
         consumer_key="ck_71555cb7c8c3489cf2ea8b231cff6ea704001ac9",
         consumer_secret="cs_8cb1c962a51cd4feac1894987d5d8ccd5aa078f3",
-        version="wc/v3"
+        version="wc/v3",
     )
-    
+
     orders = wcapi.get("orders").json()
-    
+
     customers = []
     for order in orders:
         billing_info = order.get("billing")
         if billing_info:
             customer = {
-                "id": billing_info.get("customer_id"),
                 "first_name": billing_info.get("first_name"),
                 "last_name": billing_info.get("last_name"),
                 "email": billing_info.get("email"),
-                # Agregar otros campos según sea necesario
+                "phone": billing_info.get("phone"),  # Incluir el dato del teléfono
             }
             customers.append(customer)
-    
+            # Guardar el cliente en la base de datos
+            save_customer_to_database(customer)
+
     return {"customers": customers}
+
+
+def save_customer_to_database(customer):
+    # Formatear la consulta SQL para insertar el cliente en la tabla customers
+    query = f"INSERT INTO customers (first_name, last_name, email, phone) VALUES ('{customer['first_name']}', '{customer['last_name']}', '{customer['email']}', '{customer['phone']}')"
+    # Ejecutar la consulta
+    execute_query(query)
+
 
 @app.get("/orders")
 async def read_orders():
@@ -72,9 +82,9 @@ async def read_orders():
         url="https://rizosfelices.co",
         consumer_key="ck_71555cb7c8c3489cf2ea8b231cff6ea704001ac9",
         consumer_secret="cs_8cb1c962a51cd4feac1894987d5d8ccd5aa078f3",
-        version="wc/v3"
+        version="wc/v3",
     )
-    
+
     orders = wcapi.get("orders").json()
-    
+
     return orders
